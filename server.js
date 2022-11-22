8eeb147860d72cb29b9e6808444277c769dd73bf
Athul Raj <mail@athulraj.com>
2022-11-22 18:26:40 +0530

    template features

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 var lp = require("node-lp");
+const fs = require('fs')
 const express = require('express')
 var pkginfo = require('pkginfo')(module);
+var _ = require('lodash');
 const Handlebars = require('handlebars');
 
 const app = express()
@@ -13,13 +15,13 @@ printer = lp({
 });
 
 const verifyToken = (req, res, next) => {
-	if((req.headers.authorization || "").split(' ')[1] == process.env.AUTH_KEY)
+	if ((req.headers.authorization || "").split(' ')[1] == process.env.AUTH_KEY)
 		next();
 	else
-		res.status(403).send({result: false,	error: "Unauthorized", });
+		res.status(403).send({ result: false, error: "Unauthorized", });
 };
 
-app.get('/', async (req, res) => {
+app.get('/', (req, res) => {
 	res.send({
 		service: {
 			name: module.exports.name,
@@ -27,12 +29,39 @@ app.get('/', async (req, res) => {
 			version: module.exports.version,
 			description: module.exports.description,
 		},
+		templates: fs.readdirSync('./templates/')
+			.map(function (fileName) {
+				var source_template_ht = fs.readFileSync('./templates/' + fileName, 'utf8');
+				var template = Handlebars.compile(source_template_ht);
+				return {
+					name: fileName,
+					info_url: req.protocol + "://" + req.headers.host + '/templates/' + fileName,
+					
+				};
+			})
+	})
+})
+app.get('/templates/:filename', (req, res) => {
+
+	var variables = {};
+	fs.readFileSync('./templates/' + req.params.filename, 'utf8').match(/\{\{\s+[\w\.]+\s+\}\}/g).forEach((variable) => {
+		variable_name = variable.replace('{{', '').replace('}}', '').trim();
+		_.set(variables, variable_name, "Data");
+	})
+	// _.forEach(variables_array, function (value, key) {
+	// 	_.set(variables, key, value[]);
+	// });
+	res.send({
+		template: {
+			name: req.params.filename,
+			variables
+		}
 	})
 })
 app.post('/print', verifyToken, (req, res) => {
 	print_data = Buffer.from(req.body.print_data, 'base64');
 	printer.queue(print_data, () => {
-		res.send({success: true, "message": "Printed"})
+		res.send({ success: true, "message": "Printed" })
 	});
 })
 app.post('/print-template', verifyToken, (req, res) => {
@@ -40,7 +69,7 @@ app.post('/print-template', verifyToken, (req, res) => {
 	var template = Handlebars.compile(source_template_ht);
 	var print_data = template(req.body.template.data);
 	printer.queue(print_data, () => {
-		res.send({success: true, "message": "Printed"})
+		res.send({ success: true, "message": "Printed" })
 	});
 })
 
